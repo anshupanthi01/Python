7e0d5f686c0107acae2dd8c800d01b8ef0fbab66
Anshu Panthi <anshupanthi01@gmail.com>
2025-01-11 13:08:36 +0545

    Add files via upload

---
+++ 06_conditionals.py
@@ -0,0 +1,18 @@
+# If elif else ladder
+
+a = int(input("Enter your age: "))
+
+if(a>18):
+    print("You are an adult.")
+
+elif(a<0):
+    print("Invalid age.")
+
+elif(a==0):
+    print("You are entering 0.")
+
+else:
+    print("You are not an adult.")
+
+ 
+print("End of program...")
